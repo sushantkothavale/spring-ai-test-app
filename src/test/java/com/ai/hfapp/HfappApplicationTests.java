e5a3f48c5e138d77a1aa817c331bee4fdb3d38dd
sushantkothavale <kothavale.sushant@gmail.com>
2025-11-11 23:52:25 +0530

    Changed test case scope

--- src/test/java/com/ai/hfapp/HfappApplicationTests.java
+++ src/test/java/com/ai/hfapp/HfappApplicationTests.java
@@ -1,13 +1,13 @@
 package com.ai.hfapp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest
+@MockitoBean
 class HfappApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
